e2b71c6d1c1330aab50ee725bd169868372649f5
alezsei <alezsei@yandex.ru>
2023-05-25 20:50:19 +0400

    Task19End

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿int InText(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Task19()
+{
+    int num1 = InText("Введите пятизначное число");
+    if (num1 > 9999 && num1 < 100000)
+    {
+        if ((num1 / 10000 == num1 % 10) && ((num1 % 100 ) / 10 == num1 / 1000 % 10))
+        {
+            Console.WriteLine("Палиндром");
+        }
+        else Console.WriteLine("Не палиндром");
+    }
+    else Console.WriteLine("Число не пятизначное");
+}
+Task19();
+
+
+
